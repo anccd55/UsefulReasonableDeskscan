27d89f555903c07ea572c3255074c1c2d06f02fb
anccd55 <anccd55@gmail.com>
2020-06-17 11:01:49 +0000

    delete page

--- main.py
+++ main.py
@@ -18,8 +18,5 @@ def generate_user(username):
 
 	return render_template('personal_user.html', user=username)
 
-@web_site.route('/page')
-def random_page():
-  return render_template('page.html', code=choice(number_list))
 
 web_site.run(host='0.0.0.0', port=8080)
